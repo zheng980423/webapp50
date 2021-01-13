935c8753de7f24779d1564929bc69ec6912aacc2
zwf <zwf19980423@gmail.com>
2021-01-13 22:58:35 +0800

    完成movieapp

---
+++ project17-movieapp/script.js
@@ -0,0 +1,64 @@
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b578f7f3a673cfc867b3133e13319189&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?&api_key=b578f7f3a673cfc867b3133e13319189&query=""';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+//获取初始数据
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.results);
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+  movies.forEach(movie => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = ` 
+    <img
+      src='${IMG_PATH + poster_path}'
+      alt="${title}"
+    />
+    <div class="movie-info">
+      <h3>${title}</h3>
+      <span class=${getClassByRate(vote_average)}>${vote_average}</span>
+    </div>
+    <div class="overview">
+      <h3>Overview</h3>
+      ${overview}
+    </div>
+  `;
+    main.appendChild(movieEl);
+  });
+}
+function getClassByRate(rate) {
+  if (rate >= 8.0) {
+    return 'green';
+  } else if (rate >= 6.0) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm);
+    search.value = '';
+  } else {
+    window.location.reload;
+  }
+});
